9ad9e86468e5d45a010bf3d22d6a96d1b3e70bce
Christina Fernandez <chfe8281@colorado.edu>
2025-04-04 14:10:45 -0600

    coding exercise w/ correct/incorrect message:

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -195,155 +195,6 @@ app.get('/codingExercise', (req, res) => {
   // helpers.startCountdown();
 });
 
-/*app.post('/codingExercise', async(req, res) => {
-    let input = req.body.code;
-    let input_1 = "";
-    let output_1 = "";
-    var getQuestion = `SELECT input_1, output_1 FROM coding_questions WHERE topic = '1300';`;
-    try {
-      let results = await db.one(getQuestion);
-      input_1 = results.input_1;
-      output_1 = results.output_1;
-      console.log("INPUT1", input_1);
-      console.log(results);
-      
-    } catch (err) {
-      res.redirect('/codingExercise')
-    }
-    const axios = require('axios');
-    let data = JSON.stringify({
-      "language": "python",
-      "version": "3.10.0",
-      "files": [
-        {
-          "name": "my_cool_code.js",
-          "content": `${input}\n${input_1}`
-        }
-      ],
-      "stdin": "",
-      "args": [
-        "1",
-        "2",
-        "3"
-      ],
-      "compile_timeout": 10000,
-      "run_timeout": 3000,
-      "compile_cpu_time": 10000,
-      "run_cpu_time": 3000,
-      "compile_memory_limit": -1,
-      "run_memory_limit": -1
-    });
-
-    console.log("Input", data);
-
-    let config = {
-      method: 'post',
-      maxBodyLength: Infinity,
-      url: 'https://emkc.org/api/v2/piston/execute',
-      headers: { 
-        'Content-Type': 'application/json', 
-        'Cookie': 'engineerman.sid=s%3Akvnpn0FXmlPNrj5oQAzFdWL3_PfixMdO.6tPjcuIScWntIC6%2BYY2vnbqfu5UeM664ikYYImkm8Qc'
-      },
-      data : data
-    };
-
-    axios.request(config)
-    .then((response) => {
-      console.log(JSON.stringify(response.data.run.output));
-      let output = JSON.stringify(response.data.run.output);
-      let message = "no";
-      if (output_1 == output)
-      {
-        message = "yes";
-      }
-      console.log(output);
-      res.render('pages/codingExercise.hbs', {
-        response: message
-      })
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-
-});*/
-
-/* app.post('/codingExercise', async(req, res) => {
-  let input = req.body.code;
-  let input_1 = "";
-  let input_2 = "";
-  let input_3 = "";
-  let output_1 = "";
-  let output_2 = "";
-  let output_3 = "";
-  var getQuestion = `SELECT input_1, output_1, input_2, output_2, input_3, output_3 FROM coding_questions WHERE topic = '1300';`;
-  try {
-    let results = await db.one(getQuestion);
-    input_1 = results.input_1;
-    input_2 = results.input_2;
-    input_3 = results.input_3;
-    output_1 = results.output_1;
-    output_2 = results.output_2;
-    output_3 = results.output_3;
-    console.log("INPUT1", input_1);
-    console.log(results);
-      
-  } catch (err) {
-    res.redirect('/codingExercise')
-  }
-  const axios = require('axios');
-  let data = JSON.stringify({
-    "language": "cpp",
-      "version": "10.2.0",
-      "files": [
-        {
-          "name": "my_cool_code.js",
-          "content": `${input}\n${input_1}`
-        }
-      ],
-      "stdin": "",
-      "args": [
-        "1",
-        "2",
-        "3"
-      ],
-      "compile_timeout": 10000,
-      "run_timeout": 3000,
-      "compile_cpu_time": 10000,
-      "run_cpu_time": 3000,
-      "compile_memory_limit": -1,
-      "run_memory_limit": -1
-  });
-
-  let config = {
-    method: 'post',
-    maxBodyLength: Infinity,
-    url: 'https://emkc.org/api/v2/piston/execute',
-    headers: {
-        'Content-Type': 'application/json', 
-        'Cookie': 'engineerman.sid=s%3Akvnpn0FXmlPNrj5oQAzFdWL3_PfixMdO.6tPjcuIScWntIC6%2BYY2vnbqfu5UeM664ikYYImkm8Qc'
-    },
-    data: data
-  };
-  console.log("data1", data);
-  axios.request(config)
-  .then((response) => {
-    console.log(JSON.stringify(response.data.run.output));
-    let output = JSON.stringify(response.data.run.output);
-    let message = "no";
-    if (output_1 == output)
-    {
-      message = "yes";
-    }
-    console.log(output);
-    res.render('pages/codingExercise.hbs', {
-      response: message
-    })
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-}); */
-
 app.post('/codingExercise', async(req, res) => {
   let input = req.body.code;
   let input_1 = "";
@@ -394,22 +245,30 @@ app.post('/codingExercise', async(req, res) => {
     data: data
   };
   console.log("data1", data);
-  let passed_1 = "no";
+  let passed_1 = false;
+  let passed = "Compile error!";
   axios.request(config)
   .then((response) => {
     console.log(JSON.stringify(response.data.run.output));
     let output = JSON.stringify(response.data.run.output);
+    passed = "Incorrect" + '\n' + "Output: " + output_1 + "\n Expected: " + output;
     if (output_1 == output)
     {
-      passed_1 = "yes";
+      passed_1 = true;
+      passed = "Success!";
     }
     console.log("DBAnswer", output_1);
     res.render('pages/codingExercise.hbs', {
-      response: passed_1
+      passed: passed,
+      error: !passed_1
     })
   })
   .catch((error) => {
     console.log(error);
+    res.render('pages/codingExercise.hbs', {
+      passed: passed,
+      error: true
+    })
   });
 }); 
 
