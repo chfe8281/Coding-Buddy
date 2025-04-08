807b749557d12286052822530ee211faf2e6864e
Kaitlyn Brown <jwbandsb4@gmail.com>
2025-04-08 14:17:57 -0600

    flashcards display correctly

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -404,35 +404,74 @@ app.post('/codingExercise', async(req, res) => {
   });
 }); 
 
-app.get('/flashcards', (req, res) =>{
+app.get('/flashcards', async (req, res) =>{
   // let name = "Card Group"
   // let show_front = true
   // let front = "This is the term"
   // let back = "This is the explanation"
-  // res.render('pages/flashcards', {name, show_front, front, back});
+  // res.render('pages/flashcards', {name, show_front, front, back});  
 
-  db.any(`SELECT decks.name, cards.front, cards.back FROM users
-    INNER JOIN users_to_decks
-      ON users.user_id = users_to_decks.user_id
-    INNER JOIN decks
-      ON users_to_decks.deck_id = decks.deck_id
-    INNER JOIN decks_to_cards
-      ON decks_to_cards.deck_id = decks.deck_id
-    INNER JOIN cards
-      ON cards.card_id = decks_to_cards.card_id
-    WHERE users.user_id = $1;`, [req.session.user.user_id])
-  .then(decks => {
-    console.log(decks)
-    res.render('pages/flashcards', {decks});
-  })
-  .catch(err => {
-    res.render('pages/flashcards', {
-      decks: [],
-      error: true,
-      message: err.message,
+  try {
+
+    let results = await db.task (async results => {
+      deck_info = await db.any(`SELECT decks.deck_id, decks.name FROM users
+        INNER JOIN users_to_decks
+          ON users.user_id = users_to_decks.user_id
+        INNER JOIN decks
+          ON users_to_decks.deck_id = decks.deck_id
+        WHERE users.user_id = $1;`, [req.session.user.user_id]);
+      let decks = [];
+      for(let i = 0; i < deck_info.length; i++) {
+        // console.log(deck_info[i].name);
+        cards = await db.any(`SELECT cards.front, cards.back FROM decks
+          INNER JOIN decks_to_cards
+            ON decks_to_cards.deck_id = decks.deck_id
+          INNER JOIN cards
+            ON cards.card_id = decks_to_cards.card_id
+          WHERE decks.deck_id = $1;`, [deck_info[i].deck_id]);
+        // console.log(cards);
+          decks[i] = {
+            name: deck_info[i].name,
+            id: deck_info[i].deck_id,
+            cards
+          }
+        // console.log(decks[i]);
+      }
+      return decks;
     });
-  });
-})
+    res.render('pages/flashcards', {decks: results});
+    // res.status(200).json({
+    //   results
+    // })
+  } catch (err) {
+    res.render('pages/flashcards',{
+      decks: [],
+      error: err
+    });
+  }
+
+  // db.any(`SELECT decks.name, cards.front, cards.back FROM users
+  //   INNER JOIN users_to_decks
+  //     ON users.user_id = users_to_decks.user_id
+  //   INNER JOIN decks
+  //     ON users_to_decks.deck_id = decks.deck_id
+  //   INNER JOIN decks_to_cards
+  //     ON decks_to_cards.deck_id = decks.deck_id
+  //   INNER JOIN cards
+  //     ON cards.card_id = decks_to_cards.card_id
+  //   WHERE users.user_id = $1;`, [req.session.user.user_id])
+  // .then(decks => {
+  //   console.log(decks)
+  //   res.render('pages/flashcards', {decks});
+  // })
+  // .catch(err => {
+  //   res.render('pages/flashcards', {
+  //     decks: [],
+  //     error: true,
+  //     message: err.message,
+  //   });
+  // });
+});
 
 
 app.engine('hbs', hbs.engine);
