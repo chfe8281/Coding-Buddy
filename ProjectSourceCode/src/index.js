102a5f502f9cf8d88ee92a8e8639ea5ef1c37122
Kaitlyn Brown <jwbandsb4@gmail.com>
2025-04-14 02:44:45 -0600

    fix flashcards nav and comment GET route

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -741,9 +741,13 @@ app.get('/mcq', (req, res) => {
 // <!-- End of Multiple Choice Question API Routes -->
 // *****************************************************
 
+// *****************************************************
+// <!-- Flashcards API Routes -->
+// *****************************************************
 app.get('/flashcards', async (req,res) => {
   try {
 
+    // Get user decks
     let results = await db.task (async results => {
       deck_info = await db.any(`SELECT decks.deck_id, decks.name FROM users
         INNER JOIN users_to_decks
@@ -751,6 +755,8 @@ app.get('/flashcards', async (req,res) => {
         INNER JOIN decks
           ON users_to_decks.deck_id = decks.deck_id
         WHERE users.user_id = $1;`, [req.session.user.user_id]);
+
+      // Get corresponding cards
       let decks = [];
       for(let i = 0; i < deck_info.length; i++) {
         // console.log(deck_info[i].name);
@@ -771,9 +777,6 @@ app.get('/flashcards', async (req,res) => {
       return decks;
     });
     res.render('pages/flashcards', {decks: results});
-    // res.status(200).json({
-    //   results
-    // })
   } catch (err) {
     res.render('pages/flashcards',{
       decks: [],
@@ -781,6 +784,10 @@ app.get('/flashcards', async (req,res) => {
     });
   }
 });
+
+// *****************************************************
+// <!-- End Flashcards API Routes -->
+// *****************************************************
 
 module.exports = app.listen(3000);
 console.log('Server is listening on port 3000');
