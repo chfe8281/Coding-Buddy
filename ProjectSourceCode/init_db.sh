c73b209d555f9d0f6330e66acb37b43808784b46
Christina Fernandez <Christina.Fernandez@colorado.edu>
2025-08-25 14:03:06 -0600

    Update init_db.sh

--- ProjectSourceCode/init_db.sh
+++ ProjectSourceCode/init_db.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
 
-# DO NOT PUSH THIS FILE TO GITHUB
-# This file contains sensitive information and should be kept private
-
-# TODO: Set your PostgreSQL URI - Use the External Database URL from the Render dashboard
-PG_URI="postgresql://coding_buddy_db_user:RrcXMHbbkW0juSthSDAw6exIiCvmZLcM@dpg-d2man6q4d50c73alaslg-a.oregon-postgres.render.com/coding_buddy_db"
-
-# Execute each .sql file in the directory
-for file in src/init_data/*.sql; do
-    echo "Executing $file..."
-    psql $PG_URI -f "$file"
-done
