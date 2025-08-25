89e633426caae0264dc77e2c1daea0e5cb6db68d
ninaffb04 <ninaffb2004@gmail.com>
2025-08-25 14:20:57 -0600

    change db url

--- ProjectSourceCode/init_db.sh
+++ ProjectSourceCode/init_db.sh
@@ -4,7 +4,7 @@
 # This file contains sensitive information and should be kept private
 
 # TODO: Set your PostgreSQL URI - Use the External Database URL from the Render dashboard
-PG_URI="postgresql://coding_buddy_db_user:RrcXMHbbkW0juSthSDAw6exIiCvmZLcM@dpg-d2man6q4d50c73alaslg-a.oregon-postgres.render.com/coding_buddy_db"
+PG_URI=${DATABASE_URL}
 echo "Running create.sql..."
 psql "$PG_URI" -f src/init_data/create.sql
 # Execute each .sql file in the directory
